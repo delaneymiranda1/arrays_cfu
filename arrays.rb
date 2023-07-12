a11d71e6a2c28f112b4c9a49207bcc3ec49836c9
Miranda Delaney <delaney.miranda1@gmail.com>
2023-07-12 17:31:49 -0600

    Add understanding of .length method and correction of previous work

--- arrays.rb
+++ arrays.rb
@@ -7,16 +7,19 @@ rent_paid = [true, false, false, true]
 puts names.pop
 print names
 
-# Calling .push method on the ages array will result in an item being added to the end and returns the array itself.
-puts ages.push 
+# Calling .push method on the ages array will result in an 1 being added to the end of the array.
+puts ages.push(1)
 print ages
 
 # Calling .shift method on the rent array will result in 766.25 being extracted from the rent array.
 puts rent.shift
 print rent
 
-# Calling .unshift method on the rent_paid array will result in an item being added to the beginning and returnsthe array itself.
-puts rent_paid.unshift
+# Calling .unshift method on the rent_paid array will result in 1 being added to the beginning and returnsthe array itself.
+puts rent_paid.unshift(1)
 print rent_paid 
 
 # Demonstration of understanding of index positions: Index positions are the placement in which each item in the array is. The positions start at 0. So for instance the first item is 0, second is 1, etc.
+
+# Calling .length method on the names array will result with a 3 (beacause we pulled Tamaras name with .pop). This method shows the length of the array.
+puts names.length

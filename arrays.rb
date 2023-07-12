4f19732e242654ef3c0e6092f8e6949d9c11af11
Miranda Delaney <delaney.miranda1@gmail.com>
2023-07-12 16:58:54 -0600

    Add arrays and methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ names = ["Miranda", "Ainsley", "Kayla", "Tamara"]
 ages = [27, 32, 30, 34]
 rent = [766.25, 865.32, 765.42, 834.93]
 rent_paid = [true, false, false, true]
+
+# Calling .pop method on the names array will result in Tamara.
+puts names.pop
+
+# Calling .push method on the ages array will result in the 34 being pushed and returns the array itself.
+puts ages.push 
+
+# Calling .shift method on the rent array will result in 766.25.
+puts rent.shift
+
+# Calling .unshift method on the rent_paid array will result in true being pushed and returns the array itself.
+puts rent_paid.unshift
